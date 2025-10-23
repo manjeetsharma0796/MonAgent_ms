26fc5322b248a529d52cba21f25a142b4e5f55a1
manjeet0796 <manjeetsharma0796@gmail.com>
2025-10-23 19:19:28 +0530

    specific updated required

--- lang.py
+++ lang.py
@@ -1,13 +1,14 @@
 import json
-import getpass
 import os
 from dotenv import load_dotenv
 from langchain.tools import tool
 import serpapi
 load_dotenv() 
 
-if not os.environ.get("GOOGLE_API_KEY"):
-  os.environ["GOOGLE_API_KEY"] = getpass.getpass("GOOGLE_API_KEY")
+# Check for required API key
+GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
+if not GOOGLE_API_KEY:
+    raise ValueError("GOOGLE_API_KEY environment variable is required. Please set it in your environment.")
 
 
 from langchain.chat_models import init_chat_model
